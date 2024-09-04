5f4fdd7b88cd54d24f0d4fe30649e2a0b3333aed
PatrickTle924 <72108073+PatrickTle924@users.noreply.github.com>
2024-09-03 21:27:31 -0700

    Initialize JS File
    Set up variables for JS file including array for grid and its divs

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector(".grid");
+    let squares = document.querySelectorAll(".grid div");
+    //array of 200 squares, counted from 0 to 199
+    const width = 10;
 
+    //HTML constants
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start-button");
 });
